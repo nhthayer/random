3f02da50ca11852eec30e3d1a433740c1642b06a
Nate Thayer <nhthayer@gmail.com>
2017-09-24 17:22:48 -0700

    added graphing live temps

--- arduino/temp_monitor.py
+++ arduino/temp_monitor.py
@@ -1,10 +1,9 @@
 import serial
 import time
 
-
 class temperature_controller:
     def __init__(self, which_port, verbose=True):
-        self.port = serial.Serial(which_port)
+        self.port = serial.Serial(which_port, baudrate=115200)
         self.verbose = verbose
         self.port.readline() # Blocks until start message is written
         self.temp = None
@@ -24,6 +23,13 @@ class temperature_controller:
         setattr(self, f'{which_sensor}_temp', reading)
         return reading
 
+    def get_temperature_by_index(self, which_index):
+        self.port.write(bytes('%s' % which_index, 'ascii'))
+        reading = float(
+            self.port.readline().decode('ascii').strip().split('=')[1])
+        setattr(self, f'{which_index}_temp', reading)
+        return reading
+
     def set_desired_temperature(self, which_sensor, desired_temp):
         setattr(self, f'{which_sensor}_setpoint', float(desired_temp))
 
@@ -31,9 +37,71 @@ class temperature_controller:
         self.port.close()
 
 if __name__ == '__main__':
-    sensor = temperature_controller('/dev/cu.usbmodem1421')
-    for i in range(10):
-        print('HLT:',  sensor.get_temperature('HLT'))
-        print('Mash:', sensor.get_temperature('Mash'))
-        print('Boil',  sensor.get_temperature('Boil'))
+    import matplotlib
+    matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+    
+    import tkinter as Tk
+
+    ## Create tk root object
+    root = Tk.Tk()
+    
+    ## Create plot fig, axis
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(8, 8), dpi=100)
+
+
+    ## create tk.Drawing area
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.show()
+    canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+    
+    def _quit():
+        root.quit()     # stops mainloop
+        root.destroy()
+
+    hlt_temps=[]
+    mash_temps=[]
+    boil_temps=[]
+
+    def update_temp():
+        print('Updating plot...')
+        hlt_current_temp = sensor.get_temperature('HLT')
+        mash_current_temp = sensor.get_temperature('Mash')
+        boil_current_temp = sensor.get_temperature('Boil')
+        hlt_temps.append(hlt_current_temp)
+        mash_temps.append(mash_current_temp)
+        boil_temps.append(boil_current_temp)
+
+        ax[0, 0].cla()
+        ax[0, 1].cla()
+        ax[1, 0].cla()
+
+
+        ax[0, 0].set_xlim(0, 120)
+        ax[0, 1].set_xlim(0, 120)
+        ax[1, 0].set_xlim(0, 120)
+
+        ax[0, 0].set_xticks([])
+        ax[0, 1].set_xticks([])
+        ax[1, 0].set_xticks([])
+
+        ax[0, 0].set_title('HLT Temp Probe')
+        ax[0, 1].set_title('Mash Temp Probe')
+        ax[1, 0].set_title('Boil Temp Probe')
+        
+        ax[0, 0].plot(hlt_temps[-100:])
+        ax[0, 1].plot(mash_temps[-100:])
+        ax[1, 0].plot(boil_temps[-100:])
+        canvas.show()
+        
+        root.after(10, update_temp)
+    
+    button = Tk.Button(master=root, text='Quit', command=_quit)
+    button.pack(side=Tk.BOTTOM)
+
+    sensor = temperature_controller('/dev/cu.usbmodem1411', verbose=False)
+
+    root.after(10, update_temp)
+    Tk.mainloop()
     sensor.close()
